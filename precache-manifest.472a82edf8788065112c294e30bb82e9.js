6226a18c35f58f2da9226c811b50ca40a5048c2d
Nguyễn Xuân Phú <40159538+tolaphu@users.noreply.github.com>
2020-04-27 12:41:07 +0700

    Updates

---
+++ precache-manifest.472a82edf8788065112c294e30bb82e9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef06a2e99047d626336ed82a9f97acb0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5fddec422b3335d68f07",
+    "url": "/static/css/main.be6df9eb.chunk.css"
+  },
+  {
+    "revision": "852bf9bd473ee1fe4d16",
+    "url": "/static/js/2.f146625a.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/static/js/2.f146625a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5fddec422b3335d68f07",
+    "url": "/static/js/main.3d74246d.chunk.js"
+  },
+  {
+    "revision": "e173dfe45a061c6e2715",
+    "url": "/static/js/runtime-main.20a4ba51.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/static/media/image.d7265326.png"
+  }
+]);
